25b52dea72708dd6ba43aab004f973093736db18
Tobola Jan <tobola@2n.com>
2026-05-07 19:49:22 +0200

    Add `random` property to `Id` and implement random ID generation

--- kotlin-html/src/main/kotlin/SelectorUtils.kt
+++ kotlin-html/src/main/kotlin/SelectorUtils.kt
@@ -11,7 +11,26 @@ fun Id.cssSelector() = "#${this.value}"
 fun CssClass.cssSelector() = ".${this.value}"
 
 @JvmInline
-value class Id(val value: String)
+value class Id(val value: String) {
+    companion object {
+        val random get() = randomId()
+    }
+}
 
 @JvmInline
 value class CssClass(val value: String)
+
+private fun randomId(): String {
+    val letters = ('a'..'z') + ('A'..'Z')
+    val allAllowed = letters + ('0'..'9')
+
+    // 1. Pick the first char only from letters
+    val firstChar = letters.random()
+
+    // 2. Pick the next 6 chars from letters + numbers
+    val remainingChars = (1..6)
+        .map { allAllowed.random() }
+        .joinToString("")
+
+    return firstChar + remainingChars
+}
